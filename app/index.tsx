f91e2c025ceeb5908b7e87ebd7c651c389352f67
AnthonyScott <anthony.scott1005@gmail.com>
2025-03-05 17:19:01 -0500

    Style index and remove app config using the old icon

--- app/index.tsx
+++ app/index.tsx
@@ -11,20 +11,20 @@ export default function Index() {
   const [permissionsGranted, setPermissionsGranted] = useState<boolean>(false);
 
   return (
-    <View className="bg-gray-600 h-screen flex-1 items-center justify-between p-4">
+    <View className="bg-gray-600 h-screen flex-1 items-center justify-evenly p-4">
       <Text className="text-white text-2xl">Expo Nativewind Starter</Text>
 
       {!permissionsGranted &&
       <Pressable
-        className="h-24 w-36 round-sm text-white text-2xl border-2 border-black"
+        className="h-24 w-40 rounded-lg border-2 border-black bg-black p-2"
         onPress={async () => {
           setPermissionsGranted(await requestAllPermissions())
         }
       }>
-        Request Permissions
+        <Text className="text-white text-2xl">Request Permissions</Text>
       </Pressable>
       }
-      
+
     </View>
   );
 }
