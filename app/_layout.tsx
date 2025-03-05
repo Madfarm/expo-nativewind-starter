e6127ad1dc9212c820ef2724369b3346ebef63dd
AnthonyScott <anthony.scott1005@gmail.com>
2025-03-04 20:35:48 -0500

    Update android manifest and remove ios build

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
 import { Stack } from "expo-router";
 import "../global.css"
+import 'expo-dev-client';
+
 
 export default function RootLayout() {
   return <Stack screenOptions={{ headerShown: false}} />;
