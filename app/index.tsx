c9734abbc1e7b58f9de7541612dd31b7fa958e9d
AnthonyScott <anthony.scott1005@gmail.com>
2025-03-28 17:36:39 -0400

    Remove the WhisperContext variable in favor of directly accessing the ref

--- app/index.tsx
+++ app/index.tsx
@@ -10,18 +10,18 @@ import {
 import { initWhisper } from 'whisper.rn'
 import type { WhisperContext } from "whisper.rn";
 import { fileDir, modelHost, createDir, toTimestamp } from "@/lib/util";
+import emitter from "@/lib/emitter";
 
 
 
 
 export default function Index() {
-  const whisperContextRef = useRef<WhisperContext | null>(null);
-  const whisperContext = whisperContextRef.current;
-  const [permissionsGranted, setPermissionsGranted] = useState<boolean>(false);
+  const whisperContextRef = useRef<WhisperContext | null>(null)
+  const [permissionsGranted, setPermissionsGranted] = useState<boolean>(false)
   const [logs, setLogs] = useState(["Campfire mobile"])
   const [transcibeResult, setTranscibeResult] = useState<string | null>(null)
   const transcribeResultRef = useRef<string | null>(null);
-  const stopTranscribeRef = useRef<{ stop: () => void } | null>(null);
+  const stopTranscribeRef = useRef<{ stop: () => void } | null>(null)
 
   const log = useCallback((...messages: any[]) => {
     setLogs((prev) => [...prev, messages.join(' ')])
@@ -30,6 +30,25 @@ export default function Index() {
   useEffect(() => {
     whisperContextRef.current?.release()
     whisperContextRef.current = null
+
+    emitter.addListener("stop", async () => {
+      log("Stop was detected")
+
+      const t0 = Date.now()
+      await stopTranscribeRef.current?.stop()
+      const t1 = Date.now()
+      log('Stopped transcribing in', t1 - t0, 'ms')
+
+      stopTranscribeRef.current = null
+
+      log(transcribeResultRef.current)
+
+
+
+      setTimeout(() => {
+        startTranscribe();
+      }, 2000);
+    })
   }, [])
 
   const updateTranscribeResult = useCallback((result: string) => {
@@ -53,39 +72,31 @@ export default function Index() {
 
   const startTranscribe = async () => {
     log('Start realtime transcribing...')
-    if (!whisperContext) return log('No context')
+    if (!whisperContextRef?.current) return log('No context')
     try {
       await createDir(log)
       const { stop, subscribe } =
-        await whisperContext.transcribeRealtime({
+        await whisperContextRef.current.transcribeRealtime({
           maxLen: 1,
           language: 'en',
           realtimeAudioSec: 60,
           realtimeAudioSliceSec: 25,
           audioOutputPath: "../assets/audio/temp.wav",
         })
-      stopTranscribeRef.current = { stop };
+      stopTranscribeRef.current = { stop }
       subscribe(async (evt) => {
         const { isCapturing, data, processTime, recordingTime } = evt
 
         if (data?.result.toLowerCase().includes("start")) {
-          updateTranscribeResult(data.result);
+          updateTranscribeResult(data.result)
         }
 
         if (data?.result.toLowerCase().includes("stop")) {
-          log("Stop was detected")
-
-          const t0 = Date.now()
-          await stopTranscribeRef.current?.stop();
-          const t1 = Date.now()
-          log('Stopped transcribing in', t1 - t0, 'ms')
-          stopTranscribeRef.current = null;
-
-          log(transcribeResultRef.current)
+          emitter.emit("stop", { message: "ayyy"})
         }
 
         if (!isCapturing) {
-          stopTranscribeRef.current = null;
+          stopTranscribeRef.current = null
           log('Finished realtime transcribing')
           log(transcribeResultRef.current)
         }
@@ -116,9 +127,9 @@ export default function Index() {
               log(`Permissions granted? - ${perms}`)
               setPermissionsGranted(perms)
 
-              if (whisperContext) {
+              if (whisperContextRef.current) {
                 log('Found previous context')
-                await whisperContext.release()
+                await whisperContextRef.current.release()
                 whisperContextRef.current = null
                 log('Released previous context')
               }
